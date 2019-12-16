8f3c8abeb1515150260f37087a93707d63b473b7
Laura Andrade <laura.nunes.andrade@gmail.com>
2019-12-16 12:18:55 -0300

    Update apply model (scikit_learn).

--- juicer/scikit_learn/model_operation.py
+++ juicer/scikit_learn/model_operation.py
@@ -316,8 +316,8 @@ class EvaluateModelOperation(Operation):
                      recall_score(y_true, y_pred, average='weighted')],
                     ['Accuracy', accuracy_score(y_true, y_pred)],
                     ['Cohens kappa', cohen_kappa_score(y_true, y_pred)],
-                    ['Jaccard similarity coefficient score', 
-                     jaccard_similarity_score(y_true, y_pred)],
+                    ['Jaccard coefficient score', 
+                     jaccard_score(y_true, y_pred, average='weighted')],
                     ['Matthews correlation coefficient (MCC)', 
                      matthews_corrcoef(y_true, y_pred)],
                 ]

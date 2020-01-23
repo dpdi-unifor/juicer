5bdf6d305ed65c5b36f5bfc71ee92c0c96112ac1
robsoncampelo <robson.campelo@dcc.ufmg.br>
2020-01-23 09:26:03 -0300

    Fix problems with Execute Python Code Operation

--- juicer/scikit_learn/etl_operation.py
+++ juicer/scikit_learn/etl_operation.py
@@ -440,6 +440,8 @@ class ExecutePythonOperation(Operation):
             'in2': in2,
             'out1': out1,
             'out2': out2,
+            'DataFrame': pd.DataFrame,
+            'createDataFrame': pd.DataFrame,
             
             # Restrictions in Python language
              '_write_': lambda v: v,
